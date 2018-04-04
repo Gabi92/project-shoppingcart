ce7fa62d1b40bbf328a9c4c0575a9c679b3d4781
Gabi92 <ga_engler@hotmail.com>
2018-04-04 19:27:48 +0200

    Add files via upload

---
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/NackademinShoppingcartApplication.java
@@ -0,0 +1,12 @@
+package com.schoolproject.shoppingcart.nackademinshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NackademinShoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NackademinShoppingcartApplication.class, args);
+	}
+}
