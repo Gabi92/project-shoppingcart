640184cf596cf4fbc950ef51c2f66f4d60328412
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-04-11 22:20:41 +0200

    Added VerifyUserRequest that is the first call for PIQ when initiating a payment.

--- nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/callbackhandler/PiqCallbackController.java
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/callbackhandler/PiqCallbackController.java
@@ -1,17 +1,42 @@
 package com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.VerifyUserInput;
 
 @Controller
 @RequestMapping(value="/paymentiq")
 public class PiqCallbackController {
 
-	
+	@Autowired
+	private PiqTxHandler piqTxHandler;
 	
 	@RequestMapping(value="/verifyuser", method = RequestMethod.POST)
-	public void testHomePage() {
+	public void verifyUserRequest(@RequestBody String indata, HttpServletRequest request, HttpServletResponse response) {
+		
+		try {
+			VerifyUserInput verifyUserInput = new VerifyUserInput(indata);
+			String verifyUserResponse = piqTxHandler.verifyUserHandler(verifyUserInput);
+			
+			response.setContentType("application/json");
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().write(verifyUserResponse);
+			
+			return;
+		
+		} catch (Exception e) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			e.printStackTrace();
+			return;
+		}
 	}
 	
 }
--- nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/callbackhandler/PiqTxHandler.java
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/callbackhandler/PiqTxHandler.java
@@ -1,5 +1,27 @@
 package com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler;
 
+import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.VerifyUserInput;
+
 public class PiqTxHandler {
 
+	
+	
+	public String verifyUserHandler(VerifyUserInput indata) {
+
+		String response;
+	
+		if (indata.getUserId().equals("1") && indata.getSessionId().equals("123")) {
+			
+			response = "SUCCESS";
+			
+			return response;
+			
+		} else {
+			
+			response = "FAILED";
+			
+			return response;
+			
+		}
+	}
 }
