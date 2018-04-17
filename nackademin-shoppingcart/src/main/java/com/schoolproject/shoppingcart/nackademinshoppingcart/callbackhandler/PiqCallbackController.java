92e9fbfdaefb67b5bc12561ac880af2e33669a1d
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-04-17 15:40:53 +0200

    fixed imports in PiqCallbackController

--- nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/callbackhandler/PiqCallbackController.java
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/callbackhandler/PiqCallbackController.java
@@ -3,17 +3,12 @@ package com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.AuthorizeTxInput;
-import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.CancelTxInput;
-import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.TransferTxInput;
+import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-
-import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.VerifyUserInput;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
