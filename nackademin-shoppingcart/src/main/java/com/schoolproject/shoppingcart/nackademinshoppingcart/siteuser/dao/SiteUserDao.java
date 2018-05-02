c23ea4e26d36a38f9a4f84d566c4b7399e1a7bfe
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-05-02 03:02:06 +0200

    Created UserDao for SiteUser using spring data and created UserService wich uses the DAO for saving and calling for userdata.

---
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/siteuser/dao/SiteUserDao.java
@@ -0,0 +1,13 @@
+package com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.dao;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.SiteUser;
+
+@Repository
+public interface SiteUserDao extends CrudRepository<SiteUser, Long>{
+
+	
+}
---
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/siteuser/service/SiteUserService.java
@@ -0,0 +1,19 @@
+package com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.SiteUser;
+import com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.dao.SiteUserDao;
+
+@Service
+public class SiteUserService {
+	
+	@Autowired
+	SiteUserDao siteUserDao;
+	
+	public void saveSiteUser(SiteUser siteUser) {
+		siteUserDao.save(siteUser);
+	}
+
+}
