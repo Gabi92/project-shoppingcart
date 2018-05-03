4e64423a0a49dd276411b97e70cf6a3d5784837b
Jungerstam <v.bjakov@gmail.com>
2018-05-03 11:10:00 +0200

    created validator class for users

---
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/callbackhandler/PiqCallbackValidator.java
@@ -0,0 +1,58 @@
+package com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler;
+
+import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.AuthorizeTxInput;
+import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.CancelTxInput;
+import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.TransferTxInput;
+import com.schoolproject.shoppingcart.nackademinshoppingcart.callbackhandler.callbackinput.VerifyUserInput;
+import com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.SiteUser;
+
+public class PiqCallbackValidator {
+
+    public boolean validateVerifyUserRequest(SiteUser user, VerifyUserInput indata) {
+
+        if(!indata.getUserId().equals(user.getUserId().toString())) {
+
+            return false;
+
+        } else {
+
+            return true;
+        }
+    }
+
+    public boolean validateAutorizeTxRequest(SiteUser user, AuthorizeTxInput indata) {
+
+        if(!indata.getUserId().equals(user.getUserId().toString())){
+
+            return false;
+
+        } else {
+
+            return true;
+        }
+    }
+
+    public boolean validateTranserTxRequest(SiteUser user, TransferTxInput indata) {
+
+        if(!indata.getUserId().equals(user.getUserId().toString())) {
+
+            return false;
+
+        } else {
+
+            return true;
+        }
+    }
+
+    public boolean validateCancelTxRequest(SiteUser user, CancelTxInput indata) {
+
+        if(!indata.getUserId().equals(user.getUserId().toString())) {
+
+            return false;
+
+        } else {
+
+            return true;
+        }
+    }
+}
