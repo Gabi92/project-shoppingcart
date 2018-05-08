3b79a2dff8b0af8b23fe0ff21e7a0468ea5b3770
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-05-08 23:44:29 +0200

    Added a controller for login authentication wich seems to work well.

---
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.schoolproject.shoppingcart.nackademinshoppingcart.controllers;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthController {
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	String userLogin() {
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+
+			return "redirect:/";
+		}
+
+		return "loginpage";
+	}
+
+}
