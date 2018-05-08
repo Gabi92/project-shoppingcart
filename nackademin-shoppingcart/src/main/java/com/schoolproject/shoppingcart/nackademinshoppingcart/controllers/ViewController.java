1e1fca6f0252e9ca93abb07964c08430a5d181a0
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-05-08 23:38:37 +0200

    created package controllers and moved ViewController into that package.

--- nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/controllers/ViewController.java
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/controllers/ViewController.java
@@ -1,4 +1,4 @@
-package com.schoolproject.shoppingcart.nackademinshoppingcart;
+package com.schoolproject.shoppingcart.nackademinshoppingcart.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
