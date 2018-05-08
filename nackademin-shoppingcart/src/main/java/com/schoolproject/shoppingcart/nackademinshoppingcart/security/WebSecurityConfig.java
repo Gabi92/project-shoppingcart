9da08d67e6773d3584829460ee2e5f931b47ab37
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-05-08 23:43:48 +0200

    Implemented Spring Security configuration and Authentication. log in authentication now works and added some antmatches for page restriction(still testing).

---
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/security/WebSecurityConfig.java
@@ -0,0 +1,85 @@
+package com.schoolproject.shoppingcart.nackademinshoppingcart.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.service.SiteUserService;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	SiteUserService siteUserService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		//@formatter:off
+		http
+			.authorizeRequests()
+				.antMatchers(
+					"/",
+					"/h2/**")
+				.permitAll()
+				.antMatchers(
+					"/css/*",
+					"/img/*",
+					"/js/**")
+				.permitAll()
+				.antMatchers(
+					HttpMethod.POST,
+					"/paymentiq/**")
+				.permitAll()
+			.anyRequest()
+				.authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/login")
+				.usernameParameter("email")
+				.passwordParameter("password")
+				.defaultSuccessUrl("/")
+				.permitAll()
+			.and()
+			.logout()
+				.permitAll()
+				.logoutSuccessUrl("/login");
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		//@formatter:on
+
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		/*//@formatter:off
+		auth.inMemoryAuthentication()
+		.withUser("username")
+		.password("password")
+		.roles("USER");	
+		//@formatter:on*/
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		try {
+			auth.userDetailsService(siteUserService).passwordEncoder(passwordEncoder);
+		} catch( Exception e){
+			e.printStackTrace();
+		}
+	}
+}
--- nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/siteuser/service/SiteUserService.java
+++ nackademin-shoppingcart/src/main/java/com/schoolproject/shoppingcart/nackademinshoppingcart/siteuser/service/SiteUserService.java
@@ -1,13 +1,21 @@
 package com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.SiteUser;
 import com.schoolproject.shoppingcart.nackademinshoppingcart.siteuser.dao.SiteUserDao;
 
 @Service
-public class SiteUserService {
+public class SiteUserService implements UserDetailsService {
 	
 	@Autowired
 	SiteUserDao siteUserDao;
@@ -21,5 +29,24 @@ public class SiteUserService {
 		
 		return user;
 	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
+		SiteUser user = siteUserDao.findByEmail(email);
+
+		if (user == null) {
+
+			throw new UsernameNotFoundException(email);
+
+		}
+
+		List<GrantedAuthority> auth = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+
+		String password = user.getPassword();
+
+		return new User(email, password, auth);
+
+	}
 }
+
